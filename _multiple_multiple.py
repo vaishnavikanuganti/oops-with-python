cdc672622bcc3c4c184dfd8218ff8c855ee42957
vaishnavikanuganti <vaishnavikanuganti19@gmail.com>
2026-09-21 15:00:44 +0530

    Create _multiple_multiple.py

---
+++ _multiple_multiple.py
@@ -0,0 +1,17 @@
+class calculator:
+  def add(self, a, b):
+    print("Addition:", a + b)
+
+def subtract(self, a,b):
+  print("subtraction:", a-b)
+
+def multiply(self, a,b):
+  print("multiplication:", a*b)
+
+
+calc = calculator()
+
+calc.add(10,5)
+calc.subtract(10,5)
+calc.multiply(10,5)
+              
