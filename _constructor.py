20f31c6f1b0707d3501aab0f84101ebb04cfd69d
vaishnavikanuganti <vaishnavikanuganti19@gmail.com>
2026-09-21 15:11:13 +0530

    Implement Employee class with display method
    Fix indentation and add missing attributes in Employee class.

---
+++ _constructor.py
@@ -0,0 +1,12 @@
+class Employee:
+  def__init__(self, name, salary):
+  self.salary = salary
+
+
+def display(self):
+  print("Name:",self.name)
+  print("salary:",self.salary)
+
+
+employee = Employee("vyshnavi", 30000)
+employee.display()
